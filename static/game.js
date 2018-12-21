a22fe72cdce6c0583e281adcd2cd2d2496c7db62
Triah <nicolaihedegaardjensen93@gmail.com>
2018-12-21 01:37:19 +0100

    drag and drop impl started needs alot of work

--- static/game.js
+++ static/game.js
@@ -22,9 +22,11 @@ var draggable = false;
 
 socket.emit('new player');
 
+/*
+socket on get json object that defines what should be on the canvas
+*/
 
-
-const rect = new Rectangle(10,10,100,100);
+const rect = new Rectangle(500,10,100,100);
 const circle = new Circle(60,170,50);
 const filledRect = new FilledRectangle(120,10,100,100,"blue","test", "white");
 
@@ -37,9 +39,27 @@ canvasObjects.push(filledRect);
 /*This needs to be done more clean*/
 canvasUpdated();
 
-console.log(canvasObjects[1]);
-
 //socket.emit('canvasObjects', canvasObjects);
+
+canvas.onmousedown = function(e){
+  //make sure only one item is picked
+  var itemIsLocked = false;
+  for(var i = 0; i < canvasObjects.length; i++){
+    if(!itemIsLocked && 
+      e.x+10 < canvasObjects[i].x + canvasObjects[i].w +20 && 
+      e.x+10 > canvasObjects[i].x && 
+      e.y - 10 > canvasObjects[i].y &&
+      e.y - 10 < canvasObjects[i].y + canvasObjects[i].h){
+      itemIsLocked = true;
+      //set this to variables to get the offset values and use these in another function
+      canvasObjects[i].x = e.x - 10 - canvasObjects[i].w/2;
+      canvasObjects[i].y = e.y - 10 - canvasObjects[i].h/2
+      canvasUpdated();
+    }
+  }
+  //unlock item
+  itemIsLocked = false;
+}
 
 function draw(){
   if(canvasUpToDate == false){
